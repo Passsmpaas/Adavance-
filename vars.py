31a2c2b0249c3f219f1a7b2f96956fb29775f8a9
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-07 20:07:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_ID    = os.environ.get("API_ID", "22470912")
 API_HASH  = os.environ.get("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 OWNER = os.environ.get("OWNER", "7678862761")
-AUTH_USER = os.environ.get('AUTH_USERS', '7678862761').split(',')
+AUTH_USER = os.environ.get("AUTH_USERS", "7678862761 , "7678862778").split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
