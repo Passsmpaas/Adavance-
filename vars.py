3ccdbb50788c223a64860a3dc3e886f2ff150210
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-07 19:56:49 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,6 +5,11 @@ import os
 API_ID    = os.environ.get("API_ID", "22470912")
 API_HASH  = os.environ.get("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-
+OWNER = os.environ.get("OWNER", "7678862761")
+AUTH_USER = os.environ.get('AUTH_USERS', '7678862761').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
+  
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
